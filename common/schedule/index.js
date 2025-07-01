22d6814074c44e93909ddfc5f79a45a4fbfe0665
MYPC\46745 <467456744@qq.com>
2025-07-01 18:15:55 +0800

    x

--- common/schedule/index.js
+++ common/schedule/index.js
@@ -3,12 +3,12 @@ const cron = require('node-cron');
 
 // 每分钟执行一次
 cron.schedule('* * * * *', () => {
-  console.log('每分钟执行的任务');
+  // console.log('每分钟执行的任务');
 });
 
 // 每天凌晨执行
 cron.schedule('0 0 * * *', () => {
-  console.log('每天凌晨执行的任务');
+  // console.log('每天凌晨执行的任务');
 });
 
 module.exports = {
