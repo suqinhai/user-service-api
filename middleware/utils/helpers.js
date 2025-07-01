4f071b0725c7e0d9d4dc782e9af6850413892374
MYPC\46745 <467456744@qq.com>
2025-07-01 11:23:45 +0800

    x

--- middleware/utils/helpers.js
+++ middleware/utils/helpers.js
@@ -12,33 +12,57 @@ const { logger } = require('../../common/logger');
  * @returns {Function} 组合后的中间件函数
  */
 function composeMiddlewares(...middlewares) {
-  return (req, res, next) => {
-    let index = 0;
-    
+  return function composedMiddleware(req, res, next) {
+    let index = -1;
+    let finished = false;
+
     function dispatch(i) {
+      if (finished) {
+        return Promise.resolve();
+      }
+
       if (i <= index) {
+        finished = true;
         return Promise.reject(new Error('next() called multiple times'));
       }
-      
+
       index = i;
       let fn = middlewares[i];
-      
+
       if (i === middlewares.length) {
+        finished = true;
         fn = next;
       }
-      
+
       if (!fn) {
-        return;
+        return Promise.resolve();
       }
-      
+
       try {
-        return Promise.resolve(fn(req, res, () => dispatch(i + 1)));
+        return Promise.resolve(fn(req, res, function nextWrapper(err) {
+          if (finished) {
+            return Promise.resolve();
+          }
+
+          if (err) {
+            finished = true;
+            return Promise.reject(err);
+          }
+
+          return dispatch(i + 1);
+        }));
       } catch (err) {
+        finished = true;
         return Promise.reject(err);
       }
     }
-    
-    return dispatch(0);
+
+    dispatch(0).catch(err => {
+      if (!finished && next && typeof next === 'function') {
+        finished = true;
+        next(err);
+      }
+    });
   };
 }
 
