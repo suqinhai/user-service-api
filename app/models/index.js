2c1bd6e7464ff82c6cc2dc00dd0c1e7a9387e860
DESKTOP-2U891VJ\kpskp <467456744@qq.com>
2025-07-13 17:27:45 +0800

    x

--- app/models/index.js
+++ app/models/index.js
@@ -1,17 +1,24 @@
-const { sequelize } = require('../../common/index');
+const { sequelize, sequelizeUser } = require('../../common/index');
 const userModel = require('./users/user');
 const IpBlacklistModel = require('./merchants/IpBlacklist');
 const merchantUserModel = require('./merchants/user');
+// const enhancedUserModel = require('./users/enhancedUser');
 
 // 初始化模型
 const models = {
-  userModel: userModel(sequelize),
   merchantUserModel: merchantUserModel(sequelize),
   IpBlacklistModel: IpBlacklistModel(sequelize),
+};
+
+const modelsUser = {
+  userModel: userModel(sequelizeUser),
+  // enhancedUserModel: enhancedUserModel(sequelizeUser),
 };
 
 // 导出模型和 Sequelize 实例
 module.exports = {
   ...models,
+  ...modelsUser,
   sequelize,
+  sequelizeUser,
 };
